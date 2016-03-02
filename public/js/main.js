a6d2b7638f4f9166e38bd3c3c082e23810696530
Nicolas <nicolas.rfontes@gmail.com>
2016-03-02 17:09:47 -0300

    Começando Funcionalidade de Cadastrar Evento

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 var choveuaqui = angular.module('choveuaqui', []);
 
 choveuaqui.controller('eventos', function($scope,$http){
-
+    $scope.novoEvento = {
+        nome:'',
+        email:'',
+        estado:'',
+        cidade:'',
+        dataChuva:'',
+        detalhesChuva:'',
+        localChuva:'',
+        latitudeChuva:'',
+        longitudeChuva:''
+    }
+    $scope.novoEvento.localChuva = document.getElementById('txtEndereco').value;
+    $scope.salvarNovoEvento = function(){
+        $scope.novoEvento.localChuva = document.getElementById('txtEndereco').value;
+        $scope.novoEvento.latitudeChuva = document.getElementById('txtLatitude').value;
+        $scope.novoEvento.longitudeChuva = document.getElementById('txtLongitude').value;
+        console.log($scope.novoEvento);
+    };
 });
